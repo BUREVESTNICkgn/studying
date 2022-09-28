1181fbaf555c243082a081c5805dac4e72a31365
Tigrancho <tigrantarifyan@yandex.com>
2022-09-28 23:22:45 +0500

    Initial commit

---
+++ 2.1.5.py
@@ -0,0 +1,7 @@
+class cost:
+     def __init__(self, first , second, passport):
+        self.first = first
+        self.second = second
+
+    def print_data(self):
+        print(self.name, self.balance, self.passport)
