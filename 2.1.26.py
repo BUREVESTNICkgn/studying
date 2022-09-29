f817f11709427714addb735e236768baf31e4b24
Tigrancho <tigrantarifyan@yandex.com>
2022-09-30 00:58:44 +0500

    Initial commit

--- 2.1.26.py
+++ 2.1.26.py
@@ -3,7 +3,7 @@
 1.26 Создать класс Angle для работы с углами на плоскости, 
 задаваемыми величиной в градусах и минутах.
 Обязательно должны быть реализованы:
-перевод в радианы, приведение к диапазону 0-360, 
+перевод в радианы✓, приведение к диапазону 0-360✓, 
 увеличение и уменьшение угла на заданную величину,
 получение синуса, сравнение углов.
 
@@ -15,7 +15,7 @@ class Angle:
     def __init__(self, degree, minute):
         global flag 
         flag = True
-        if str(degree).isnumeric() == False or str(minute).isnumeric() == False:
+        if str(degree).isnumeric() == False or str(minute).isnumeric() == False: 
             flag = False
         else:
             self.degree = int(degree)
@@ -26,14 +26,20 @@ class Angle:
         rad1 = self.degree * math.pi / 180 
         rad2 = self.minute * math.pi / 10800
         return rad1 + rad2
-        # def dd(self):
-        #     degrees = math.trunc(decimal_degrees)
-        #     minutes = math.trunc((decimal_degrees - degrees) * 60)
-        #     return degrees, minutes
+    def re_dd(self):
+        degrees = math.trunc(self.decimal_degrees())
+        minutes = math.trunc((self.decimal_degrees() - degrees) * 60)
+        return degrees, minutes
+    def sin(self):
+        return math.sin(self.radian())
+
 a = input('A: ')
 b = input('B: ')
 s = Angle(a, b)
 print(s.radian())
+print(s.decimal_degrees())
+print(s.re_dd())
+print(s.sin())
 
 """
 https://en.wikipedia.org/wiki/Decimal_degrees
