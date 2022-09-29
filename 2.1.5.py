b89e4302793c22ff65ed29ea744696bb3860166d
Tigrancho <tigrantarifyan@yandex.com>
2022-09-29 08:36:30 +0500

    Initial commit

--- 2.1.5.py
+++ 2.1.5.py
@@ -1,13 +1,26 @@
+# 1.5 Поле first – дробное положительное число, цена товара; 
+# поле second – целое положительное число, количество единиц товара.
+# Реализовать метод cost() – вычисление стоимости товара.
+
+
 class table:
     def __init__(self, first, second):
-
-        self.first = first
-        self.second = second
-
+        global flag 
+        flag = True
+        if str(first).isnumeric() == False:
+            print('Не корректный ввод')
+            flag = False
+        else:
+            self.first = float(first)
+            self.second = int(second)
+            
     def cost(self):
-        return self.first * self.second
+        if flag: 
+            return self.first * self.second
+        else:
+            pass
 
-first = float(input('Введите левую границу диапазона: '))
-second = int(input('Введите правую границу диапазона: '))
+first = input('Введите цену: ')
+second = input('Введите кол-во: ')
 f = table(first, second)
 print(f.cost())
