ed122ca7c1e753def679ca683deb05e63a15ffa4
Tigrancho <tigrantarifyan@yandex.com>
2022-10-02 11:30:39 +0500

    Initial commit

--- 2.1.26.py
+++ 2.1.26.py
@@ -10,27 +10,32 @@
 """
 
 import math
-import P
+import PyQt5
+
 
 class OperOnDegrees:
     def __init__(self, degree, minute):
         self.degree = int(degree)
         self.minute = int(minute)
         pass
+
     def __add__(self, other):
         new1 = OperOnDegrees(self.degree, self.minute)
         new2 = OperOnDegrees(self.degree, self.minute)
         new1.degree += other.degree
         new1.minute += other.minute
-        return new1, new2 
+        return new1, new2
+
     def __sub__(self, other):
         pass
+
     def transfer(self):
         if self.minute >= 60:
             order_minute = self.minute // 60
             self.minute = self.minute - (order_minute * 60)
             self.degree += order_minute
         return self.degree, self.minute
+
     def dif(self):
         pass
 
@@ -45,24 +50,29 @@ class Angle:
     def __init__(self, degree, minute):
         global flag 
         flag = True
-        if str(degree).isnumeric() == False or str(minute).isnumeric() == False: 
+        if str(degree).isnumeric() == False or str(minute).isnumeric() == False:  # noqa: E712
             flag = False
         else:
             self.degree = int(degree)
             self.minute = int(minute)
+
     def decimal_degrees(self):
         return math.degrees(self.radian())
+
     def radian(self):
         rad1 = self.degree * math.pi / 180 
         rad2 = self.minute * math.pi / 10800
         return rad1 + rad2
+
     def re_dd(self):
         degrees = math.trunc(self.decimal_degrees())
         minutes = math.trunc((self.decimal_degrees() - degrees) * 60)
         return degrees, minutes
+
     def sin(self):
-        'sin(rad)'
+        """sin(rad)"""
         return math.sin(self.radian())
+
 
 a = input('A: ')
 b = input('B: ')
