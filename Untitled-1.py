ff39ae4950d7c8122306cdcfbfd697ab089edfbc
Tigrancho <tigrantarifyan@yandex.com>
2022-10-08 12:32:05 +0500

    Initial commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,16 +1,28 @@
 class OperOnDegrees:
     
-    def __init__(self, qwe):
-        self.qwe = qwe
+    # def __init__(self, qwe):
+    #     self.qwe = qwe
+
+    # def __add__(self, other):
+    #     return OperOnDegrees(self.qwe + other.qwe)
+    
+
+    def __init__(self, degree, minute):
+        self.degree = int(degree)
+        self.minute = int(minute)
+        pass
 
     def __add__(self, other):
-        return OperOnDegrees(self.qwe + other.qwe)
+        new1 = OperOnDegrees(self.degree, self.minute)
+        new1.degree += other.degree
+        new1.minute += other.minute
+        return new1
     
     def get(self):
-        return self.qwe
+        return self.degree, self.minute
     
-a = OperOnDegrees(5)
-b = OperOnDegrees(12)
+a = OperOnDegrees(5, 4)
+b = OperOnDegrees(12, 5)
 
 c = a + b
 
